f1cf692e1a48477a482e4da382a1cb94be9ebc32
Takumi Sato <satotaku.kamigou@gmail.com>
2018-01-10 04:44:14 +0900

    chefとrecipeのmodelとtestを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109181901) do
+ActiveRecord::Schema.define(version: 20180109183557) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180109181901) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
